b05e5078dfaa8da524eb3909b13e34c7d8716b4e
SCPSIX <45662937+scpxin@users.noreply.github.com>
2026-06-07 00:09:08 +0800

    Add backend/src/main/java/com/his/mapper/NursingRecordMapper.java

---
+++ backend/src/main/java/com/his/mapper/NursingRecordMapper.java
@@ -0,0 +1,9 @@
+package com.his.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.his.entity.NursingRecord;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface NursingRecordMapper extends BaseMapper<NursingRecord> {
+}
