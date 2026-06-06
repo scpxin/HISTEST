43f511d4850f92ab56167ead2608cd4288a1744a
SCPSIX <45662937+scpxin@users.noreply.github.com>
2026-06-07 00:09:10 +0800

    Add backend/src/main/java/com/his/mapper/ScheduleMapper.java

---
+++ backend/src/main/java/com/his/mapper/ScheduleMapper.java
@@ -0,0 +1,9 @@
+package com.his.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.his.entity.Schedule;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+}
